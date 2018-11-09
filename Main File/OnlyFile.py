afe1486aaf7637821d7670c31a4e281fb3a7da23
TimHenkins <43077776+TimHenkins@users.noreply.github.com>
2018-11-09 12:16:51 -0800

    Update OnlyFile.py

--- Main File/OnlyFile.py
+++ Main File/OnlyFile.py
@@ -13,7 +13,7 @@ inventory = []
 def invento(a):
     inventory.append(a)
 
-r=1
+
 while Cell == 1:
     print "You awaken from your bed on a cold and stormy night to find that your cell door has been mysteriously left open."
     print "You wonder if this is a trap or if this is your lucky day."
@@ -26,41 +26,45 @@ while Cell == 1:
         Cell = 0
         Hall2 = 1
 
+    if ("inspect"and "room") in moov:
+        print "Over in the corner you see a toilet that connects to the sewer system and opposite of that is your bed and desk"
+    if ("inspect" and "desk")in moov:
+        print "There is a drawer in your desk that could be opened"
+    if ("open" and "drawer")in moov:
+        print "inside the desk you find a note. It reads,"
+        print " In order to leave the prison, ypu have two options. Either brute force your way through the front door, or escape through the sewer system which puts you out in the forest outside of the prison."
+        print "The choice is yours. Either way, Best of Luck and Happy Hunting."
+        print "Your ally,"
+        print "anonymous"
 
-t = 0
-p = 1
+
+
+
+
+
+
 while Hall2 == 1:
-  u = 1
-  if p == 1:
     print "Standing in front of your cell, you see a cell block in front of you, a hall to your left and right, and your cell behind you."
-    p = 0
-  if u == 1:
-    dohall = raw_input("what do you do?")
-    u = 0
-  if ("move" and "left") in dohall:
-    print "You move to the left hall"
-    Hall2 = 0
-    Hall1 = 1
-  if ("move" and "right") in dohall:
-    print "You move the hall to your right"
-    Hall2 = 0
-    Hall3 = 1
-  if ("move" and "forward") in dohall:
-    print "You move into the cell block in front of you"
-    Hall2 = 0
-    CellB1 = 1
-  if ("move" and "backwards" or "back") in dohall:
-    print "You move back into your cell"
-    Hall2 = 0
-    Cell = 1
-  if ("inspect" and "hall" or "room") in dohall:
-    print "You see a small tile slightly raised compared to the rest of the floor."
-  if ("inspect" and "tile" or "small tile") in dohall:
-    print "You lift a small tile off the floor revealing a small satchel of a slurry of all drugs"
-    t = 1
-  if (t == 1 and "take" and "satchel of drugs" or "satchel" or "drugs") in dohall:
-    print "you take the small satchel of drugs"
-    invento("drugs")
+    if u == 1:
+        dohall = raw_input("what do you do?")
+        u = 0
+    if ("move" and "left") in dohall:
+        print "You move to the left hall"
+        Hall2 = 0
+        Hall1 = 1
+    if ("move" and "right") in dohall:
+        print "You move the hall to your right"
+        Hall2 = 0
+        Hall3 = 1
+    if ("move" and "forward") in dohall:
+        print "You move into the cell block in front of you"
+        Hall2 = 0
+        CellB1 = 1
+    if ("move" and "backwards" or "back") in dohall:
+        print "You move back into your cell"
+        Hall2 = 0
+        Cell = 1
+    
 
 while Hall1 == 1:
     y = 1
@@ -70,74 +74,30 @@ while Hall1 == 1:
         y = 0
     if "move" and "left" in move:
         print "You move into the cell block"
-        CBlock = 1
+        C_Block = 1
         Hall1 = 0
-    if ("move" and "left") in move:
+    if "move" and "left" in move:
         print "you move into the showers, don't drop the soap..."
-        print "You see a perfectly ordinary room of showers"
         Shower = 1
         Hall1 = 0
-    if ("move" and "left") in move:
+    if "move" and "left" in move:
         print "you move back into the hall outside your cell"
         Hall2 = 1
         Hall1 = 0
-    
+        
 while Hall3 == 1:
     print "you enter a long hall, to your left and right are two other halls, directly in front of you is the door to the yard"
     if me == 1:
         mo = raw_input("What do you do?")
         me = 0
-    if ("move" and "left") in move:
+    if "move" and "left" in mo:
         Hall3 = 0
         Hall4 = 1
-    if ("move" and "right") in move:
+    if "move" and "right" in mo:
         Hall3 = 0
         HallO = 1 
-
-while HallO == 1:
-    print 'You enter a hall, to your right is the canteent, to your left is the kitchen"
-    if j ==1:
-        muuv = raw_input("What do you do")
-        j = 0
-    if ("move" and "left") in muuv:
-        print "you move into the kitchen"
-        HallO = 0
-        kitchen = 1
-    if ("move" and "right") in muuv:
-        print "You move into the canteen"
-        HallO = 0
-        canteen = 1
-        
-ugh_killme = 1
-while canteen == 1:
-    if ugh_killme ==1
-        buehler = raw_input("What do you do?")
-        ugh_killme = 0
-    if ("move" and "outside") in buehler:
-        print "You move out into the hall, to your right is the canteent, to your left is the kitchen"
-        HallO = 1
-        canteen = 0
-        
-wee = 1
-while yard == 1:
-    if wee == 1:
-        ack = raw_input("What do you do?")
-        wee = 0
-    if ("move" and "back") in ack:
-        print "You move back into the hall,to your front is the yard, to your left and right are halls, behind you is another hallway to your cell"
-        yard = 0
-        Hall3 = 1
-        
-hodor = 1
-while shower == 1:
-    if hodor ==1
-        knute = raw_input("What do you do?")
-        hodor = 0
-    if ("move" and "outside") in knute:
-        print "you move outside, to your left is the shower, and to your right is the canteen"
-        shower = 0
-        HallO = 1
     
+        
  
   
         
